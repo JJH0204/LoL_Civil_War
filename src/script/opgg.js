d97f5f3c76eeb05f5fa9a4ed970d690a35ffcd01
KRJaeh0 <145985933+JJH0204@users.noreply.github.com>
2025-12-30 19:28:55 +0900

    Refactor code structure for improved readability and maintainability

--- src/script/opgg.js
+++
@@ -1,6 +0,0 @@
-function openOpggMulti() {
-    if(!players.length) return alert("플레이어 없음");
-    window.open(`https://www.op.gg/multisearch/kr?summoners=${encodeURIComponent(players.map(p=>p.name).join(','))}`, '_blank');
-}
-
-window.openOpggMulti = openOpggMulti;
